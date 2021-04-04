ce7fe522a67654d06c0c6f54666cc9f4fe80fa4f
Michel Leblond <michel.leblond62@orange.fr>
2021-04-04 21:13:24 +0200

    Update French translation deleted: po/content.fr-fr.mo modified: po/content.fr-fr.po po/content.pot tools/generate-translations.sh

--- tools/generate-translations.sh
+++ tools/generate-translations.sh
@@ -49,13 +49,13 @@ for section in content; do
     echo "[po4a_paths] po/${section}.pot \$lang:po/${section}.\$lang.po" >> $po4a_conf
     cat >> $po4a_conf <<EOF
 
-[options] opt:"--addendum-charset=UTF-8" opt:"--localized-charset=UTF-8" opt:"--master-charset=UTF-8" opt:"--msgmerge-opt='--no-wrap'" opt:"--porefs=file" opt:"--wrap-po=newlines" opt:"--master-language=en_US"
+[options] opt:"--addendum-charset=UTF-8" opt:"--localized-charset=UTF-8" opt:"--master-charset=UTF-8" opt:"--msgmerge-opt='--no-wrap'" opt:"--porefs=file" opt:"--wrap-po=newlines" opt:"--master-language=en_US" #opt:"--keep=10"
 
 [po4a_alias:markdown] text opt:"--option markdown" opt:"--option yfm_keys=title" opt:"--addendum-charset=UTF-8" opt:"--localized-charset=UTF-8" opt:"--master-charset=UTF-8" opt:"--option neverwrap"
 
 EOF
 
-    # for f in $section/*.md; do
+    #for f in $section/*.md; do
     for f in  $(find content -type f -name '*.md'); do
 	echo "[type: markdown] $f \$lang:$(dirname $f)/$(basename $f .md).\$lang.md" >> $po4a_conf
     done
